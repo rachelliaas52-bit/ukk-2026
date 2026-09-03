a47e682eb1e10389cef1c57334c25ce34ab87f58
rachelliaas52-bit <rachelliaas52@gmail.com>
2026-09-03 16:17:53 +0700

    pembaruan

--- app/Controllers/TarifController.php
+++ app/Controllers/TarifController.php
@@ -4,11 +4,13 @@ namespace App\Controllers;
 
 use Sakuci\Controller;
 use Sakuci\Http\Request;
+use App\Models\Tarif;
 
 class TarifController extends Controller
 {
     public function index(Request $request)
     {
-        return "tarif harga";
+        $data= Tarif::paginate(5);
+        return view('tarif.index', compact('data'));
     }
 }
---
+++ resources/views/tarif/index.sakuci.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title', config('app.name')  .  ' --kerangka PHP ringan')
+
+@section ('content')
+
+    <h1>Daftar Tarif</h1>
+
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>id_tarif</th>
+            <th>jenis_kendaraan</th>
+            <th>tarif_per_jam</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($data as $d)
+        <tr>
+            <td> {{ $d->id_tarif }} </td>
+            <td> {{ $d->jenis_kendaraan }} </td>
+            <td> {{ $d->tarif_per_jam }} </td>
+            <td><button class="btn btn-sm btn-success">Edit</button>
+            <button class="btn btn-sm btn-danger">Hapus</button>
+        </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{!! $data->links() !!}
+
+@endsection
